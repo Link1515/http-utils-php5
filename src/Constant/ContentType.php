45a94ef5f926ff50e34d8c17f973b63149a6032f
Link1515 <h26281660@gmail.com>
2024-03-19 10:56:17 +0800

    add ContentType constants

---
+++ src/Constant/ContentType.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Link1515\HttpUtils\Constant;
+
+class ContentType
+{
+  public const JSON = "application/json";
+  public const XML = "application/xml";
+  public const X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
+  public const FORM_DATA = "multipart/form-data";
+  public const PLAIN = "text/plain";
+}
